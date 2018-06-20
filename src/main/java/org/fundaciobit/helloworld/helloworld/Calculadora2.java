516088fd6c4090465c7cffcce057a183727ac697
Francesc Bosch <francescboschmartorell@gmail.com>
2018-06-20 13:29:14 +0200

    Fer versio 2 #3

---
+++ src/main/java/org/fundaciobit/helloworld/helloworld/Calculadora2.java
@@ -0,0 +1,14 @@
+package org.fundaciobit.helloworld.helloworld;
+
+public class Calculadora2 extends Calculadora{
+	
+	 public static void main( String[] args )
+	    {
+	        System.out.println( "Hello World 2 !" );
+	    }
+	    
+	    public int resta(int a, int b) {
+	    	return a-b;
+	    }
+
+}
